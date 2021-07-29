6c470cda6dc77e48f1ec703e723748fb9862aa0b
Daniel Park <dpark0703@gmail.com>
2021-07-28 23:41:12 -0500

    Add most_common function
    - Call libraries to assist
    - Create function and call in main.py

--- helpers.py
+++ helpers.py
@@ -3,6 +3,36 @@
 # Desc: Helper functions used in processing main and camera
 
 from datetime import datetime
+import itertools
+import operator
+
+def most_common(frequency_list):
+	"""
+	Gets the most common element in a list
+	
+	Args:
+		(list) frequency_list: list of the five name_codes detected when iterating
+	
+	Returns:
+		The most frequent element in the list
+
+	"""
+	# get an iterable of (item, iterable) pairs
+	SL = sorted((x, i) for i, x in enumerate(L))
+	# print 'SL:', SL
+	groups = itertools.groupby(SL, key=operator.itemgetter(0))
+	# auxiliary function to get "quality" for an item
+	def _auxfun(g):
+		item, iterable = g
+		count = 0
+		min_index = len(L)
+		for _, where in iterable:
+			count += 1
+			min_index = min(min_index, where)
+		# print 'item %r, count %r, minind %r' % (item, count, min_index)
+		return count, -min_index
+	# pick the highest-count/earliest item
+	return max(groups, key=_auxfun)[0]
 
 def mark_attendance(person_name):
     """
